4d489f79b69619ae7ab0980e689db403a6eac449
Surya742 <suryakumarsahani.742@gmail.com>
2020-11-04 19:34:45 +0530

    Updates

---
+++ precache-manifest.feb6553f80fa494c435962da323c4afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da0c4661e810425194918aceaea8183",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "225c7457045ae57e8f67",
+    "url": "/FaceRecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bc6015abaff449cb45b8",
+    "url": "/FaceRecognition/static/css/main.80f540c5.chunk.css"
+  },
+  {
+    "revision": "225c7457045ae57e8f67",
+    "url": "/FaceRecognition/static/js/2.ceebf226.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognition/static/js/2.ceebf226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6015abaff449cb45b8",
+    "url": "/FaceRecognition/static/js/main.1ec76f95.chunk.js"
+  },
+  {
+    "revision": "9081a6be4b07f1443e68",
+    "url": "/FaceRecognition/static/js/runtime-main.6496c0e8.js"
+  }
+]);
